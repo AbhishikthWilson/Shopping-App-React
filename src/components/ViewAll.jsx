a58f97c4a3bb17ef8446950664c7b25574af82a9
AbhishikthWilson <abhishikthwilson@gmail.com>
2024-06-09 13:02:18 +0530

    view all complete

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const ViewAll = () => {
+  return (
+    <div>
+
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <div class="card">
+  <img  src="https://imgs.search.brave.com/bs6ZmwWbGtXwTP6iAnyuvTYfXpbXDer-ynWs0jx2K0g/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/MDM2MDI2NDI0NTgt/MjMyMTExNDQ1NjU3/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4T0h4OGNI/SnZaSFZqZEh4bGJu/d3dmSHd3Zkh4OE1B/PT0.jpeg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Product</h5>
+    <p class="card-text">Rate: Rs 500</p>
+    <a href="#" class="btn btn-primary">Buy Now</a>
+  </div>
+</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default ViewAll
